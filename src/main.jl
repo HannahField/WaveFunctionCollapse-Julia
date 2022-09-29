797395c521490b18d87b95a53ee18540241b2fa6
Hannah <thathannahfield@gmail.com>
2022-09-29 22:59:57 +0200

    Initial commit

---
+++ src/main.jl
@@ -0,0 +1,84 @@
+struct Tile
+    northSocketID::UInt8
+    eastSocketID::UInt8
+    southSocketID::UInt8
+    westSocketID::UInt8
+    tileID::UInt8
+end
+
+mutable struct Cell
+    currentTiles::Vector{Tile}
+    northCompatible::Vector{Tile}
+    eastCompatible::Vector{Tile}
+    southCompatible::Vector{Tile}
+    westCompatible::Vector{Tile}
+    collapsed::Bool
+end
+
+mutable struct Grid
+    cells::Matrix{Cell}
+end
+
+
+tiles = [
+    Tile(1, 0, 1, 1, 1),
+    Tile(1, 1, 0, 1, 2),
+    Tile(1, 1, 1, 0, 3),
+    Tile(0, 1, 1, 1, 4),
+    Tile(0, 0, 0, 0, 5)
+]
+
+struct TileCompatibility
+    tile::Tile
+    northNeighbors::Vector{Tile}
+    eastNeighbors::Vector{Tile}
+    southNeighbors::Vector{Tile}
+    westNeighbors::Vector{Tile}
+end
+
+function findCompatibility(tile, tiles)
+    northCompatibility = filter(x -> x.southSocketID == tile.northSocketID, tiles)
+    eastCompatibility = filter(x -> x.westSocketID == tile.eastSocketID, tiles)
+    southCompatibility = filter(x -> x.northSocketID == tile.southSocketID, tiles)
+    westCompatibility = filter(x -> x.eastSocketID == tile.westSocketID, tiles)
+    return TileCompatibility(tile, northCompatibility, eastCompatibility, southCompatibility, westCompatibility)
+end
+
+tileCompatibility = Dict(map(x -> (x, findCompatibility(x, tiles)), tiles))
+
+
+size = (4, 4)
+
+baseCell() = Cell(tiles, tiles, tiles, tiles, tiles, false)
+
+grid = Grid(map(_ -> baseCell(), ones(size)))
+
+function propagate!(grid, index)
+
+end
+
+function collapse!(grid)
+    index = findLeastEntropy(grid)
+    leastEntropyCell = grid.cells[index]
+
+    possibleOutcomes = leastEntropyCell.currentTiles
+    outcome = rand(possibleOutcomes)
+    grid.cells[index].currentTiles = outcome
+    propagate!(grid, index)
+end
+
+function findLeastEntropy(grid)
+    _, index = findmin(map(x -> length(x.currentTiles), filter(x -> !x.collapsed, grid.cells)))
+    return index
+end
+
+function findNeighborsIndex(index, size)
+    indeces = [
+        (index[1] - 1, index[2])
+        (index[1], index[2] + 1)
+        (index[1] + 1, index[2])
+        (index[1], index[2] - 1)
+    ]
+    filter!(x -> x[1] in 1:size[1] && x[2] in 1:size[2], indeces)
+    return indeces
+end
